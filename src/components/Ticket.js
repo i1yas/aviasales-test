74815bef3871d426e05dfdae2ab497a6cab83d37
i1yas <ilyasers@gmail.com>
2017-09-10 20:29:01 +0300

    Add basic styles (task1)

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import '../css/ticket.css';
+
+const PlaceInfo = ({ data }) => {
+  const { time, short_name, full_name, date } = data;
+  return (
+  <div className='place-info'>
+    <div className='place-info__time'>{time}</div>
+    <div className='place-info__name'>{short_name}, {full_name}</div>
+    <div className='place-info__date'>{date}</div>
+  </div>
+)};
 
 const Ticket = ({ data }) => {
   const {
     origin, origin_name,
     destination, destination_name,
     departure_date, departure_time,
+    arrival_date, arrival_time,
     carrier, stops, price
   } = data;
   
@@ -13,17 +25,20 @@ const Ticket = ({ data }) => {
       <div className='ticket__carrier'>
         {carrier}
       </div>
-      <button className='ticket__cto'>
+      <button className='ticket__cta'>
         Купить за {price} Р
       </button>
     </div>
     <div className='ticket__main'>
-      <div className='ticket__origin'>
-        <div className='ticket__time'>{departure_time}</div>
-        <div className='ticket__name'>
-          {origin}, {origin_name}
-        </div>
-      </div>
+      <PlaceInfo data={{
+        time: departure_time, short_name: origin,
+        full_name: origin_name, date: departure_date
+      }} />
+      <div className='ticket__stops'>Пересадок: {stops}</div>
+      <PlaceInfo data={{
+        time: arrival_time, short_name: destination,
+        full_name: destination_name, date: arrival_date 
+      }} />
     </div>
   </div>
   
--- src/index.js
+++ src/index.js
@@ -3,5 +3,4 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import tickets from './tickets.json';
 
-console.log(typeof tickets);
 ReactDOM.render(<App data={tickets}/>, document.getElementById('root'));
