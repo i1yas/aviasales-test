aeef49e72af2a2b1affcfb0e784ca96e28fde345
i1yas <ilyasers@gmail.com>
2017-09-20 17:39:25 +0300

    Put formating funcs in separate module

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -1,55 +1,10 @@
 import React from 'react';
-import moment from 'moment-with-locales-es6';
 import BuyButton from './BuyButton';
 import '../css/ticket.css';
 import airplaneIcon from '../airplane.svg';
-
-moment.locale('ru');
-
-const formatTime = time => {
-  const [ hour, min ] = String(time).split(':')
-  
-  const formattedHour = hour < 10 ? '0' + hour : hour;
-  
-  return `${formattedHour}:${min}`;
-}
-
-const formatDate = date => {
-  const d = new Date(date);
-  const formattedArr = moment(d).format('llll').split(' ').slice(0, 4);
-  formattedArr[2] = formattedArr[2].slice(0, 3);
-
-  let day = formattedArr[0];
-  day = day.slice(0, 2);
-  day = ', ' + day.slice(0,1).toUpperCase() + day.slice(1);
-
-  return formattedArr.slice(1).join(' ') + day;
-}
-
-const formatPrice = price => {
-  const strPrice = String(price);
-
-  if(strPrice.length < 5) return strPrice;
-
-  const splitPoint = strPrice.length % 3;
-  const headPart = strPrice.slice(0, splitPoint);
-  const restPart = strPrice.slice(splitPoint);
-
-  const arrayOfRangs = restPart.match(/[0-9]{3}/g);
-  const formattedPrice = `${headPart}${headPart && ' '}${arrayOfRangs.join(' ')}`;
-
-  return formattedPrice;
-}
-
-const getStopWord = n => {
-  if(n === 1) {
-    return 'пересадка';
-  } else if(n < 5) {
-    return 'пересадки';
-  } else {
-    return 'пересадок';
-  }
-}
+import {
+  formatDate, formatPrice, formatTime, formatStopWord
+} from '../utils/ticketFormatters';
 
 const PlaceInfo = ({ data }) => {
   const { time, short_name, full_name, date } = data;
@@ -87,7 +42,7 @@ const Ticket = ({ data }) => {
       <div className='ticket__stops'>
         <div className='ticket__stops-sign'>
           {stops > 0 ?
-            stops + ' ' + getStopWord(stops)
+            stops + ' ' + formatStopWord(stops)
             :
             ' '
           }
---
+++ src/utils/ticketFormatters.js
@@ -0,0 +1,47 @@
+import moment from 'moment-with-locales-es6';
+moment.locale('ru');
+
+export const formatTime = time => {
+  const [ hour, min ] = String(time).split(':')
+  
+  const formattedHour = hour < 10 ? '0' + hour : hour;
+  
+  return `${formattedHour}:${min}`;
+}
+
+export const formatDate = date => {
+  const d = new Date(date);
+  const formattedArr = moment(d).format('llll').split(' ').slice(0, 4);
+  formattedArr[2] = formattedArr[2].slice(0, 3);
+
+  let day = formattedArr[0];
+  day = day.slice(0, 2);
+  day = ', ' + day.slice(0,1).toUpperCase() + day.slice(1);
+
+  return formattedArr.slice(1).join(' ') + day;
+}
+
+export const formatPrice = price => {
+  const strPrice = String(price);
+
+  if(strPrice.length < 5) return strPrice;
+
+  const splitPoint = strPrice.length % 3;
+  const headPart = strPrice.slice(0, splitPoint);
+  const restPart = strPrice.slice(splitPoint);
+
+  const arrayOfRangs = restPart.match(/[0-9]{3}/g);
+  const formattedPrice = `${headPart}${headPart && ' '}${arrayOfRangs.join(' ')}`;
+
+  return formattedPrice;
+}
+
+export const formatStopWord = n => {
+  if(n === 1) {
+    return 'пересадка';
+  } else if(n < 5) {
+    return 'пересадки';
+  } else {
+    return 'пересадок';
+  }
+}
