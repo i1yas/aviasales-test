5e22be563060ab9a00a0c902a9c55c82fe72ef3a
i1yas <ilyasers@gmail.com>
2017-10-08 17:29:22 +0300

    Add styles for Filter and FilterElement

--- src/components/FilterElement.js
+++ src/components/FilterElement.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import checkMark from '../resources/check-mark.svg';
+
+const checkboxStyle = {
+	backgroundImage: `url(${checkMark})`
+}
 
 const FilterElement = ({ text, ind }) => {
 	return (
-		<div key={ind} className='filter__element'>
-			{text}
-		</div>
+		<label key={ind} className='filter__element'>
+			<i className='filter__checkbox' style={checkboxStyle}></i>
+			<input type='checkbox' className='filter__input-checkbox' />
+			<span className='filter__element-text'>{text}</span>
+		</label>
 	);
 };
 
