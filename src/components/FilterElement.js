2025d4f24a24b99ef4623fd8aaa715815d2d7796
i1yas <ilyasers@gmail.com>
2017-10-09 20:59:57 +0300

    Add styles for checkbox on checked/unchecked states

--- src/components/FilterElement.js
+++ src/components/FilterElement.js
@@ -8,8 +8,8 @@ const checkboxStyle = {
 const FilterElement = ({ text, ind }) => {
 	return (
 		<label key={ind} className='filter__element'>
-			<i className='filter__checkbox' style={checkboxStyle}></i>
 			<input type='checkbox' className='filter__input-checkbox' />
+			<i className='filter__checkbox' style={checkboxStyle}></i>
 			<span className='filter__element-text'>{text}</span>
 		</label>
 	);
